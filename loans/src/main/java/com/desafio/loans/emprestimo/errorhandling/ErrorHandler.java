08dda7f41ac09cdc1f56de9c9105c0b5e0b2ad52
KappaThrash <danielkuhin@gmail.com>
2026-04-11 13:53:04 -0300

    Add MethodArgumentNotValidException handler

--- loans/src/main/java/com/desafio/loans/emprestimo/errorhandling/ErrorHandler.java
+++ loans/src/main/java/com/desafio/loans/emprestimo/errorhandling/ErrorHandler.java
@@ -3,6 +3,8 @@ package com.desafio.loans.emprestimo.errorhandling;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,6 +19,29 @@ public class ErrorHandler {
                 OffsetDateTime.now(),
                 HttpStatus.UNPROCESSABLE_CONTENT.value(),
                 "IllegalArgumentException",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(
+                OffsetDateTime.now(),
+                HttpStatus.UNPROCESSABLE_CONTENT.value(),
+                "MethodArgumentNotValidException",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).body(errorResponse);
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex, HttpServletRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(
+                OffsetDateTime.now(),
+                HttpStatus.UNPROCESSABLE_CONTENT.value(),
+                "HttpMessageNotReadableException",
                 ex.getMessage(),
                 request.getRequestURI()
         );
